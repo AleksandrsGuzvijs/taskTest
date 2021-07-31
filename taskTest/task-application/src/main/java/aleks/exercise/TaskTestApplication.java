be839b4fe7135fd5c37ab492e92e76234d92f5a2
AleksandrsGuzvijs <superharek@yandex.ru>
2021-07-31 19:16:44 +0300

    Implemented Swagger

--- taskTest/task-application/src/main/java/aleks/exercise/TaskTestApplication.java
+++ taskTest/task-application/src/main/java/aleks/exercise/TaskTestApplication.java
@@ -2,11 +2,39 @@ package aleks.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication(scanBasePackages = "aleks")
 public class TaskTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTestApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfig(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("aleks.exercise.web"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails(){
+		return new ApiInfo(
+				"Task Manager API",
+				"2nd test application for managing tasks",
+				"1.0",
+				null,
+				new springfox.documentation.service.Contact("Aleksandrs Guzvijs",null,"aleks.guzvijs@yandex.ru"),
+				null,
+				null,
+				Collections.emptyList());
 	}
 }
--- taskTest/task-application/src/main/java/aleks/exercise/web/TaskController.java
+++ taskTest/task-application/src/main/java/aleks/exercise/web/TaskController.java
@@ -2,6 +2,8 @@ package aleks.exercise.web;
 
 import aleks.exercise.model.Task;
 import aleks.exercise.service.TaskService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,30 +20,35 @@ public class TaskController {
         private TaskService taskService;
 
         @GetMapping("/all")
+        @ApiOperation(value = "Returns all existing tasks.")
         public ResponseEntity<List<Task>> getAllTasks(){
             List<Task> tasks = taskService.getAllTasks();
             return new ResponseEntity<>(tasks, HttpStatus.OK);
         }
 
         @GetMapping("/find/{id}")
-        public ResponseEntity<Task> getTaskById(@PathVariable("id") Long id){
+        @ApiOperation(value = "Finds task by id", notes = "Provide id to find specific task in the system", response = Task.class)
+        public ResponseEntity<Task> getTaskById(@ApiParam(value = "id value of that task that you want to find.", required = true) @PathVariable("id") Long id){
             Task task = taskService.findTaskById(id);
             return new ResponseEntity<>(task, HttpStatus.OK);
         }
 
         @PostMapping("/add")
+        @ApiOperation(value = "Add new task.")
         public ResponseEntity<Task> addTask(@Valid @RequestBody Task task) {
             Task newTask = taskService.addTask(task);
             return new ResponseEntity<>(newTask, HttpStatus.CREATED);
         }
 
         @PutMapping("/update")
+        @ApiOperation(value = "Update existing task", notes = "Currently done by providing id inside of json request with the rest of the task class, also able to add new tasks through this operation")
         public ResponseEntity<Task> updateTask(@RequestBody Task task) {
             Task updateTask = taskService.updateTask(task);
             return new ResponseEntity<>(updateTask, HttpStatus.OK);
         }
 
         @DeleteMapping("/delete/{id}")
+        @ApiOperation(value = "Deletes task specified by id")
         public ResponseEntity<?> deleteTask(@PathVariable("id") Long id) {
             taskService.deleteTask(id);
             return new ResponseEntity<>(HttpStatus.OK);
